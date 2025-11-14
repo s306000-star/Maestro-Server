3730779851ad36a6fecd915ce58c3f72a580628b
s306000-star <ss30600085@gmail.com>
2025-11-14 20:36:55 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+echo "🔄 Synchronizing Telegram sessions from GitHub..."
+python3 sessions_syncy.py
+
+echo "🚀 Starting Maestro server..."
 gunicorn app:app --bind 0.0.0.0:$PORT --worker-class gevent --workers 1
